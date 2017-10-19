a72fc1df9cd867648c99c38c7fce452e48e58a96
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-19 17:06:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   holiday_supplies[:fall] = {:columbus_day => ["Flags", "Italian Food", "Parade Floats"]}
+  holiday_supplies[:winter] = {:valentines_day => ["Flags", "Italian Food", "Parade Floats"]}
 
 end
 
