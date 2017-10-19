73166deac6f2189ea2684251e65cda938ce638a3
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-19 20:31:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_supplies.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-         holiday =  holiday.to_s.split("_").collect do |w| 
-           w.capitalize!}.join(" ")
+         holiday =  holiday.to_s.split("_").collect do |w|
+           w.capitalize!.join(" ")
          end
          supplies = supplies. join(", ")
          puts "#{holiday}: #{supplies}"
